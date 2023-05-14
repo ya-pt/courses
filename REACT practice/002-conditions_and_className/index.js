4bf1e29c30dc340e432ddae653abd560b31beb86
ya-pt <caramel.pie.78@gmail.com>
2023-05-14 06:55:07 +0300

    delete my-app

--- REACT practice/002-conditions_and_className/index.js
+++ REACT practice/002-conditions_and_className/index.js
@@ -1,3 +1,8 @@
+//React
+//ReactDOM - обращаемся ко всей html странице и выбираем из нее нужные объекты
+//render - что будем помещать(только один родительский тег) в определенный html тег
+// JSX является частью библиотеки React.
+
 const helpText = "help text !";
 const elements = (
   <div className="thisIsClass">
