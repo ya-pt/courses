df3bea3127e546f5cc27b151c89298899fd5c2ab
ya-pt <caramel.pie.78@gmail.com>
2023-05-16 09:13:39 +0300

    aded new App.js file

---
+++ REACT practice/my-app/src/App.js
@@ -0,0 +1,68 @@
+//React 18
+//импортируем библиотеки
+import React from "react";
+
+// (FUNCTION COMPONENTS)-------------------------------------------------------------------------------------------
+
+const helpText = "help text !";
+
+// Component 2
+function Header(){
+  return(
+    <header>header</header>
+  );
+}
+
+// Component 1
+function MyApp() {// должно начинаться с заглавной буквы
+  return (
+    <div className="thisIsClass">
+      <Header />
+      <Header />
+      <Header />
+
+      <h1>{helpText}</h1>
+      {/* conditions */}
+      <p>{helpText === "help text !" ? "yes" : "no"}</p> {/* yes */}
+      <p>{helpText === "help text" ? "yes" : "no"}</p> {/* no */}
+      <span>___________________</span>
+    </div>
+  );
+}
+// В React для определения класса CSS в элементе используется атрибут className вместо class.
+
+
+
+// (CLASS COMPONENTS)-------------------------------------------------------------------------------------------
+
+class Header2 extends React.Component{ // наследуются от базового класса React.Component
+  render(){ // функция(метод класса)
+    return(
+      // props является ссылкой на объект, содержащий все переданные свойства
+      <header>{this.props.title}</header>
+    )
+  }
+}
+
+class MyApp2 extends React.Component{
+  text = "click or mouse"
+  render(){
+    return(<div>
+    <Header2 title="шапка сайта"/>{/*Свойство*/}
+    <Header2 title="header"/>{/*Свойство*/}
+    <Header2 title="шапка/header"/>{/*Свойство*/}
+
+    {/* this. используется для доступа к объектам, которые определены внутри класса  */}
+    <h1>{this.text}</h1>
+    <input placeholder={this.text} onClick={this.inputClick} onMouseEnter={this.mouseOver}></input>
+    </div>)
+  }
+
+  inputClick(){console.log("clicked")};
+  mouseOver(){console.log("mouse")};
+}
+
+
+//экспортируем чтобы импортировать
+export default MyApp2
+export {MyApp}
--- REACT practice/my-app/src/index.js
+++ REACT practice/my-app/src/index.js
@@ -3,76 +3,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+// импортируем созданные компоненты 
+import MyApp2, {MyApp} from './App'
 
-// (FUNCTION COMPONENTS)-------------------------------------------------------------------------------------------
-
-const helpText = "help text !";
-
-// Component 2
-function Header(){
-  return(
-    <header>header</header>
-  )
-}
-
-// Component 1
-function MyApp() {// должно начинаться с заглавной буквы
-  return (
-    <div className="thisIsClass">
-      <Header />
-      <Header />
-      <Header />
-
-      <h1>{helpText}</h1>
-      {/* conditions */}
-      <p>{helpText === "help text !" ? "yes" : "no"}</p> {/* yes */}
-      <p>{helpText === "help text" ? "yes" : "no"}</p> {/* no */}
-      <span>___________________</span>
-    </div>
-  );
-}
-// В React для определения класса CSS в элементе используется атрибут className вместо class.
-
-const app = document.querySelector("#app");
-
-const root = ReactDOM.createRoot(app);//для управления корневым компонентом вашего приложения
-root.render(<MyApp />);
-
-//ReactDOM - обращаемся ко всей html странице и выбираем из нее нужные объекты
-//render - что будем помещать(только один родительский тег) в определенный html тег
-
-
-
-
-// (CLASS COMPONENTS)-------------------------------------------------------------------------------------------
-
-class Header2 extends React.Component{ // наследуются от базового класса React.Component
-  render(){ // функция(метод класса)
-    return(
-      // props является ссылкой на объект, содержащий все переданные свойства
-      <header>{this.props.title}</header>
-    )
-  }
-}
-
-class MyApp2 extends React.Component{
-  text = "click or mouse"
-  render(){
-    return(<div>
-    <Header2 title="шапка сайта"/>{/*Свойство*/}
-    <Header2 title="header"/>{/*Свойство*/}
-    <Header2 title="шапка/header"/>{/*Свойство*/}
-
-    {/* this. используется для доступа к объектам, которые определены внутри класса  */}
-    <h1>{this.text}</h1>
-    <input placeholder={this.text} onClick={this.inputClick} onMouseEnter={this.mouseOver}></input>
-  </div>)
-  }
-
-  inputClick(){console.log("clicked")}
-  mouseOver(){console.log("mouse")}
-}
-
+// (CLASS COMPONENTS)
 const app2 = document.querySelector("#app2");
 const root2 = ReactDOM.createRoot(app2);
 root2.render(<MyApp2 />);
+
+// (FUNCTION COMPONENTS)
+const app = document.querySelector("#app");
+const root = ReactDOM.createRoot(app);//для управления корневым компонентом вашего приложения
+root.render(<MyApp />);
+//ReactDOM - обращаемся ко всей html странице и выбираем из нее нужные объекты
+//render - что будем помещать(только один родительский тег) в определенный html тег
