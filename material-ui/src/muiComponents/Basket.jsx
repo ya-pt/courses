7a88cde0e8f76ec62547d4323c22b7c64768d017
ya-pt <caramel.pie.78@gmail.com>
2023-05-31 16:52:32 +0300

    comment for continue

--- material-ui/src/muiComponents/Basket.jsx
+++ material-ui/src/muiComponents/Basket.jsx
@@ -1,23 +1,23 @@
-import { Drawer, Link, ListItem, ListItemIcon, ListItemText } from "@mui/material"
-import ShoppingBasket from '@mui/icons-material/ShoppingBasket';
+// import { Drawer, Link, ListItem, ListItemIcon, ListItemText } from "@mui/material"
+// import ShoppingBasket from '@mui/icons-material/ShoppingBasket';
 
-const Basket = (props) => {
-    const {cartOpen, closeCart = Function.prototype, order, removeFromOrder} = props; //С помощью destructuring, из объекта props извлекаются два свойства: order и removeFromOrder.
+// const Basket = (props) => {
+//     const {cartOpen, closeCart = Function.prototype, order, removeFromOrder} = props; //С помощью destructuring, из объекта props извлекаются два свойства: order и removeFromOrder.
 
-    // строим на основе material
-    return(
-        <Drawer anchor='right' open={cartOpen} onClose={closeCart}>
-            <Link>
-                <ListItem>
-                    <ListItemIcon>
-                        <ShoppingBasket/>
-                    </ListItemIcon>
-                    <ListItemText primary="корзина"/>
-                </ListItem>
-            </Link>
+//     // строим на основе material
+//     return(
+//         <Drawer anchor='right' open={cartOpen} onClose={closeCart}>
+//             <Link>
+//                 <ListItem>
+//                     <ListItemIcon>
+//                         <ShoppingBasket/>
+//                     </ListItemIcon>
+//                     <ListItemText primary="корзина"/>
+//                 </ListItem>
+//             </Link>
 
-        </Drawer>
-    )
-}
+//         </Drawer>
+//     )
+// }
 
-export default Basket
+// export default Basket
